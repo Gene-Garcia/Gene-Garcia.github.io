92dce2c165227479a2b40b8b0307ebff9ac222d2
gn-grc <genejogarcia.gg@gmail.com>
2021-07-03 11:57:07 +0800

    uploaded webp file type for website images, and deleted PNGs and JPGs

--- resources/scripts/nav-color.js
+++ resources/scripts/nav-color.js
@@ -1,23 +1,23 @@
 window.onscroll = () => {
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
 
     var yScroll = window.scrollY;
 
     // so that if the y position does not meet the if statements
     // it will be set back to its default style
-    removeNavClasses();
+    // removeNavClasses();
 
-    if (yScroll >= 939 && yScroll < 1916){
-        $('#nav-div').addClass("education");
-    } else if (yScroll >= 1916 && yScroll < 4243){
-        $('#nav-div').addClass('projects');
-    } else if (yScroll >= 4243 && yScroll < 5841){
-        $('#nav-div').addClass('papers');
-    } else if (yScroll >= 5841 && yScroll < 7726){
-        $('#nav-div').addClass('experience');
-    } else if (yScroll >= 7726){
-        $('#nav-div').addClass('skills');
-    }
+    // if (yScroll >= 939 && yScroll < 1916){
+    //     $('#nav-div').addClass("education");
+    // } else if (yScroll >= 1916 && yScroll < 4243){
+    //     $('#nav-div').addClass('projects');
+    // } else if (yScroll >= 4243 && yScroll < 5841){
+    //     $('#nav-div').addClass('papers');
+    // } else if (yScroll >= 5841 && yScroll < 7726){
+    //     $('#nav-div').addClass('experience');
+    // } else if (yScroll >= 7726){
+    //     $('#nav-div').addClass('skills');
+    // }
 
 
 };
