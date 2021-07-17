de52bcab2dff5a6f4d559c475963964d5413d5d6
gn-grc <genejogarcia.gg@gmail.com>
2021-07-17 15:37:02 +0800

    changed logic on identifying and changing active menu

--- resources/scripts/navbar.js
+++ resources/scripts/navbar.js
@@ -1,37 +1,50 @@
 window.onscroll = () => {
-    var yScroll = window.scrollY;
 
 
-    // bug
-    // on smaller screen, y scroll proximity also must change
-
-    if (yScroll >= 0 && yScroll < 939){
-        // removeNavClasses first before adding active class
-        // to introduction nav link
-        // because the scroll is still in the area of introduction
-        // introduction nav link must still have the active class,
-        // having it first allows other classes with active class
-        // to be removed, but re-adds it only to introduction
+    if ($('#introduction-div').isInViewport()) {
+        
         removeNavClasses();
         $('#nav-div .nav-link.introduction').addClass("active");
-    } else if (yScroll >= 939 && yScroll < 1900){
+
+    } else if ($('#education-div').isInViewport()) {
+        
         removeNavClasses();
         $('#nav-div .nav-link.education').addClass("active");
-    } else if (yScroll >= 1900 && yScroll < 3000){
+
+    } else if ($('#projects-div').isInViewport()) {
+        
         removeNavClasses();
         $('#nav-div .nav-link.projects').addClass('active');
-    } else if (yScroll >= 300 && yScroll < 4700){
+
+    } else if ($('#papers-div').isInViewport()) {
+        
         removeNavClasses();
         $('#nav-div .nav-link.papers').addClass('active');
-    } else if (yScroll >= 4700 && yScroll < 5535){
+
+    } else if ($('#experiences-div').isInViewport()) {
+        
         removeNavClasses();
         $('#nav-div .nav-link.experiences').addClass('active');
-    } else if (yScroll >= 5535){
+
+    } else if ($('#tech-stack-div').isInViewport()) {
+        
         removeNavClasses();
         $('#nav-div .nav-link.tech-stack').addClass('active');
-    }
 
+    }
 };
+
+// ref https://stackoverflow.com/questions/20791374/jquery-check-if-element-is-visible-in-viewport
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+}
+
 
 function removeNavClasses(){
     // removes only the "active" class from 
